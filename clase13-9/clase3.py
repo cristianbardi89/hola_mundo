fc4f1393b804ec770e36a3e9fff349928e9747fa
cristian <nahuelbardi0@gmail.com>
2023-11-29 20:15:48 -0300

    Add files via upload

---
+++ clase13-9/clase3.py
@@ -0,0 +1,21 @@
+##DESCARGAMOS GIT  DE LA PAGINA
+
+#INSTALARLO - Chequeando que este tildado GIT BASH y GIT GUI, Seleccionando Visual Studio Code as default editor - El resto todo next
+
+#Configuramos GIT con nuestro nombre escribiendo: git config --global user.name "Nuestro nombre"
+#Configuramos GIT con nuestro email escribiendo: git config --global user.email micorreo@correo.com
+
+#Abrimos la terminal en el VSC y luego abrimos la consola de Git Bash
+
+#Iniciamos git escribiendo: git init
+
+# En GIT podemos identificar 3 estados - Working Directory -> Staging Area -> Repository
+
+
+#OTROS CAMBIOS
+
+
+# git remote add origin [pegar el enlace del repo]
+
+# git push -u origin master
+
---
+++ clase13-9/clase_git.py
@@ -0,0 +1,12 @@
+""" 
+git init                                      // INICIAMOS GIT
+git add .                                    // AGREGAMOS LOS CAMBIOS AL STAGING AREA 
+git commit -m "motivo del commit"           // COMMITEAMOS LOS CAMBIOS 
+git remote add origin https://github.com/nombre_usuario/nombre_repo // AGREGAMOS EL REPO
+git push -u origin master // PUSHEAMOS EL REPO
+
+
+git pull --rebase origin main    // SI CREARON EL README AL CREAR EL REPOSITORIO Y LUEGO CAMBIARON A LA MAIN
+
+
+"""
